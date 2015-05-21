932403c2be3752bbe10dda225a3fb93e6cf370c8
Howard Dutton <hjd1964@gmail.com>
2015-05-21 08:25:32 -0400

    Fixed sync bug

--- Command.ino
+++ Command.ino
@@ -174,11 +174,13 @@ void processCommands() {
       
 //   C - Sync Control
 //  :CS#   Synchonize the telescope with the current right ascension and declination coordinates
-//  :CM#   Synchonize the telescope with the current database object (as above)
 //         Returns: Nothing
+//  :CM#   Synchonize the telescope with the current database object (as above)
+//         Returns: "N/A"
       if ((command[0]=='C') && ((command[1]=='S') || command[1]=='M'))  {
         if (trackingState!=TrackingMoveTo) {
           syncEqu(newTargetRA,newTargetDec);
+          if (command[1]=='M') strcpy(reply,"N/A");
           quietReply=true;
         }
       } else 
